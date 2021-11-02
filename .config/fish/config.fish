508d8bc95c87cd95162ce87a75f5ba29103bc141
Albert Klinkovský <albert.klinkovsky@gmail.com>
2021-11-02 18:55:08 +0100

    Add alias to fish config for vim -> nvim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 fish_add_path ~/.local/bin/
 fish_add_path ~/.cargo/bin/
 
+alias vim="nvim"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     and not set -q TMUX
